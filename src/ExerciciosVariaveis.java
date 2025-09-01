eb75fef048e7c8b47a8f5a188bba9cb5f3a3f225
thiagovscode <southiagovasconcelos@gmail.com>
2025-09-01 20:14:25 -0300

    first commit

---
+++ src/ExerciciosVariaveis.java
@@ -0,0 +1,10 @@
+public class ExerciciosVariaveis {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Olá, mundo! Estou rodando meu primeiro programa em Java.");
+    }
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,40 @@
+public class Pessoa
+
+{
+    private String nome;
+    private String idade;
+    private Double altura;
+    private Boolean estudante;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Boolean getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(Boolean estudante) {
+        this.estudante = estudante;
+    }
+}
