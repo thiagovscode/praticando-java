d1747939b32e3f60e112c99ba393f90395da89fe
thiagovscode <southiagovasconcelos@gmail.com>
2025-10-06 22:27:11 -0300

    adicionando exercicio colecoes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import colecoesestream.ExercicioColecoes;
 
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
 
+        ExercicioColecoes exer1 = new ExercicioColecoes();
+        exer1.getFuncionarios();
+        System.out.println(exer1.getFuncionarios());
+
+        exer1.setFuncionarios(Arrays.asList("Thiago", "Jeane", "Leonardo", "Alexandre", "Maria"));
+        System.out.println(exer1.getFuncionarios());
 
 
 
---
+++ src/colecoesestream/ExercicioColecoes.java
@@ -0,0 +1,16 @@
+package colecoesestream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExercicioColecoes {
+    private List<String> funcionarios = new ArrayList<>();
+
+    public List<String> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<String> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
