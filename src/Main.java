3ccd1b48134918772a694896a718cbc01f4c5bbc
thiagovscode <southiagovasconcelos@gmail.com>
2025-10-16 23:27:54 -0300

    fazendo novos exercicios

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
-import exercicios.sistema_de_pamentos.Boleto;
-import exercicios.sistema_de_pamentos.Cartao;
-import exercicios.sistema_de_pamentos.Pagamento;
-import exercicios.sistema_de_pamentos.Pix;
+import exercicios.enviando_mensagens.Mensagem;
 
 public class Main {
     public static void main(String[] args) {
 
+        Mensagem notificacao = new Mensagem();
 
-        Pagamento cartao = new Cartao(250.00);
-        Pagamento boleto = new Boleto(500.00);
-        Pagamento pix = new Pix(300.00);
 
-        cartao.confirmarPagamento();
-        boleto.confirmarPagamento();
-        pix.confirmarPagamento();
+        notificacao.enviarMensagem("Olá!");
+        notificacao.enviarMensagem("João", "Bom dia!");
+        notificacao.enviarMensagem("Maria", "Atenção!", 2);
     }
 
 
--- src/exercicios/enviando_mensagens/Mensagem.java
+++ src/exercicios/enviando_mensagens/Mensagem.java
@@ -35,21 +35,21 @@ public class Mensagem {
 
     public void enviarMensagem(String mensagem){
 
-        System.out.println("Mensagem enviada para todos: "+getMensagem());
+        System.out.println("Mensagem enviada para todos: "+ mensagem);
 
     };
 
 
     public void enviarMensagem( String destinatario,String mensagem){
 
-        System.out.println("Mensagem para "+getDestinatario() + ":"+ getMensagem());
+        System.out.println("Mensagem para "+ destinatario + ":"+ mensagem);
 
     };
 
     public void enviarMensagem( String destinatario,String mensagem, Integer frequencia){
 
         while(frequencia>0) {
-            System.out.println("Mensagem para " + getDestinatario() + ":" + getMensagem());
+            System.out.println("Mensagem para " + destinatario + ":" + mensagem);
          frequencia--;
         }
     };
